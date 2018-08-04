6dc40588aa041ade3d9ab04c0ba86d4c327b315c
Anki <anki@Ankis-MacBook-Pro.local>
2018-08-04 15:48:21 -0700

    prompt for input

--- src/train.py
+++ src/train.py
@@ -1,5 +1,10 @@
 import extract
+import webbrowser
 
 
+posts = extract.query_craigslist()
 
-extract.query_craigslist()
+for post in posts:
+    webbrowser.open(post['url'])
+    res = input('Give me a value');
+    print(res)
